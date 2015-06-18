14e7f6929b19983291e9608748ebd2825e40ae56
nathan <nathan@meraki.com>
2015-06-18 22:33:14 +0000

    Haystack: Add to settings
    This configures Haystack to use elasticsearch on its default port, and also to re-index in real time.

--- biostar/settings/ikarem.py
+++ biostar/settings/ikarem.py
@@ -16,3 +16,13 @@ DATABASES = {
         'PORT': 5432,
      }
 }
+
+HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
+
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://127.0.0.1:9200/',
+        'INDEX_NAME': 'biostar',
+    },
+}
