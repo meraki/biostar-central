a44b68728af7b03b7a4f7e78053357bee6b4d631
Sam Gruber <sam.gruber@meraki.com>
2015-06-25 20:48:35 +0000

    Set minimum post length to 10 characters

--- biostar/apps/posts/views.py
+++ biostar/apps/posts/views.py
@@ -75,7 +75,7 @@ class LongForm(forms.Form):
     )
 
     content = forms.CharField(widget=forms.Textarea,
-                              min_length=80, max_length=15000,
+                              min_length=10, max_length=15000,
                               label="Enter your post below")
 
     def __init__(self, *args, **kwargs):
